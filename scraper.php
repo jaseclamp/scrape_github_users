958b3f8b9ad0f381fe49860a627c8f68376e949f
Jase Clamp <jase.clamp@gmail.com>
2015-05-04 11:58:39 +1000

    first go

--- scraper.php
+++ scraper.php
@@ -1,26 +1,169 @@
 <?
-// This is a template for a PHP scraper on morph.io (https://morph.io)
-// including some code snippets below that you should find helpful
 
-// require 'scraperwiki.php';
-// require 'scraperwiki/simple_html_dom.php';
-//
-// // Read in a page
-// $html = scraperwiki::scrape("http://foo.com");
-//
-// // Find something on the page using css selectors
-// $dom = new simple_html_dom();
-// $dom->load($html);
-// print_r($dom->find("table.list"));
-//
-// // Write out to the sqlite database using scraperwiki library
-// scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-//
-// // An arbitrary query against the database
-// scraperwiki::select("* from data where 'name'='peter'")
+echo $MORPH_SESSION; die;
 
-// You don't have to do things with the ScraperWiki library. You can use whatever is installed
-// on morph.io for PHP (See https://github.com/openaustralia/morph-docker-php) and all that matters
-// is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
-// has at least a table called data.
+require 'rb.php';
+require 'simple_html_dom.php';  
+R::setup('sqlite:data.sqlite');
+
+//R::nuke();
+
+$topics = array('PHP','JavaScript','CSS');
+$locations = array('brisbane','sydney','qld','nsw','australia','zealand','queensland','new+south+wales','victoria','melbourne');
+$baseurl = 'https://github.com';
+
+//these values need to be updated from your browser
+$opts = array(
+            'http'=>array(
+                'method'=>"GET",
+                'header'=>
+                    "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\n".
+                    "Accept-Encoding: gzip, deflate\r\n".
+                    "Accept-Language: en-US,en;q=0.5\r\n".
+                    "Connection: keep-alive\r\n".
+                    "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:37.0) Gecko/20100101 Firefox/37.0\r\n".
+                    "Cookie: logged_in=yes; _ga=GA1.2.1030011918.1427507368; _octo=GH1.1.1923536842.1427507370; user_session=".$MORPH_SESSION."; dotcom_user=jaseclamp; tz=Australia%2FBrisbane"
+            )
+        );
+        
+$context = stream_context_create($opts);
+
+//load uids into array to save 1000s of sqlite queries
+$uids = R::getAll('select uid from data');
+if(count($uids)<1) $uids = array();
+else {
+foreach($uids as $uid) $_uids[] = $uid['uid'];
+	$uids = $_uids; unset($_uids);
+}
+
+foreach($topics as $topic):
+foreach($locations as $location):
+
+    $curr = $topic . " :: " . $location;
+
+	//results limited to 1k so specific is good.
+	$url = 'https://github.com/search?utf8=%E2%9C%93&q=language%3A'.$topic.'+location%3A'.$location.'&type=Users&ref=advsearch';
+
+	//get first page users.. recurse by looking at pagination next
+	getUsers($url);
+
+
+endforeach;
+endforeach;
+
+
+
+function getUsers($url){
+
+	$html = file_get_contents($url);
+	$dom = new simple_html_dom();
+	$dom->load($html);
+
+	foreach($dom->find('div.user-list-item') as $user)
+	{
+		$uid = substr( $user->find('a[href^=/]',0)->href , 1 ); 
+
+        echo "\n" . $GLOBALS['curr'] . " : " . $uid;
+
+		//skip if have
+		if( in_array( $uid , $GLOBALS['uids'] )  ) { echo " -- already have"; continue; }
+            
+        $users = R::dispense('data');
+        $users->url = $GLOBALS['baseurl'] . '/' . $uid;
+        $users->uid = $uid;
+        
+        //go detail page, easier to get rest of info from that.
+        if( getUserDetail($users) > 0 )
+        {    
+           $GLOBALS['uids'][] = $uid;
+           echo " -- saved";
+        }
+
+        die;
+
+	}
+
+    //is there another page??? get that beast.
+    if($nextpage = $dom->find('div.pagination a.next_page',0)->href)
+        getUsers($GLOBALS['baseurl'].$nextpage);
+
+}
+
+
+function getUserDetail($users)
+{
+    $html = file_get_contents( $users->url );
+    $dom = new simple_html_dom();
+    $dom->load($html);
+
+    $users->name = $dom->find('span.vcard-fullname',0)->plaintext;
+    $users->location = $dom->find('li[itemprop=homeLocation]',0)->plaintext;
+    $users->worksfor = $dom->find('li[itemprop=worksFor]',0)->plaintext;
+    $users->url = $dom->find('li[itemprop=url] a',0)->href;
+    $users->joined = $dom->find('time.join-date',0)->datetime;
+
+    $users->folowers = $dom->find('div.vcard-stats a.vcard-stat',0)->find('strong',0)->plaintext;
+    $users->starred = $dom->find('div.vcard-stats a.vcard-stat',1)->find('strong',0)->plaintext;
+    $users->following = $dom->find('div.vcard-stats a.vcard-stat',2)->find('strong',0)->plaintext;
+
+    $users->contributions = preg_replace( "/[^\d]/", "", $dom->find('div.contrib-column-first span.contrib-number',0) );
+
+    return R::store($users);
+
+}
+
+
+
+
+function geocodeUsers () {
+    
+    if( isset( $GLOBALS['OVER_QUERY_LIMIT'] ) ) return false;
+    
+    $users = R::getAll("select * from data where lat = ?", array('') );
+    
+    foreach($users as $user){
+        
+        if($user['lat'] != '') continue; //redundant check
+        if($user['lng'] != '') continue; 
+        if($user['location']=='') continue; 
+        
+        echo "\n".$GLOBALS['toc']." Geocoding ".$user['name']." at ".$user['location'];
+        
+        //if we got it before reuse.
+        $result = R::findOne( 'data', ' lat != :x AND lat != :blank AND location = :loc ', array( ':x'=>'XXX', ':blank'=>'', ':loc'=> $user['location'] ) );
+        //if the result is not null that means its already in the db so continue on to the next one in this loop
+        if(!is_null($result)) { 
+            echo " -- already got"; 
+            $lat = $result->lat;
+            $lng = $result->lng;
+        }else{
+            $addr = urlencode($user['location']);
+            $addr = str_replace("%2C","",$addr);
+            $url = 'http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address='.$addr;
+            $get = file_get_contents($url);
+            $records = json_decode($get,TRUE);
+            
+            if ( $records['status'] == 'OK' ) {
+                //neat_r($records['results'][]);
+                $lat = $records['results'][0]['geometry']['location']['lat'];
+                $lng = $records['results'][0]['geometry']['location']['lng'];
+                echo " -- ".$lat."-".$lng."";
+            } elseif ( $records['status'] == 'OVER_QUERY_LIMIT' ) {
+                echo " -- OVER_QUERY_LIMIT";
+                $GLOBALS['OVER_QUERY_LIMIT'] = true;
+                return false;
+            }else{
+                echo " -- XXX";
+                //neat_r($records); die;
+                $lat = 'XXX'; 
+                $lng = 'XXX'; 
+            }
+        }
+        $_users = R::load('data',$user['id']);
+        $_users->lat = $lat; 
+        $_users->lng = $lng; 
+        R::store($_users); 
+    } 
+}
+
 ?>
